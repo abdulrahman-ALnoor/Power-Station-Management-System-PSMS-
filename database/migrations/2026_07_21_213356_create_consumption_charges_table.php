c318e0b263c08246e45a57a4bfe16d51d1bb630c
ALnoor321 <Abdurahmanalnoor3@gmail.com>
2026-07-29 00:25:16 +0300

    add UserController and routes

--- database/migrations/2026_07_21_213356_create_consumption_charges_table.php
+++ database/migrations/2026_07_21_213356_create_consumption_charges_table.php
@@ -15,30 +15,24 @@ return new class extends Migration
 
         Schema::create('consumption_charges', function (Blueprint $table) {
             $table->id();
-
             $table->foreignId('customer_id')
                 ->constrained()
                 ->cascadeOnDelete();
-
             $table->foreignId('meter_id')
                 ->constrained()
                 ->cascadeOnDelete();
-
             $table->foreignId('meter_reading_id')
                 ->unique()
                 ->constrained()
                 ->cascadeOnDelete();
-
             $table->decimal('total_amount', 12, 2);
             $table->decimal('paid_amount', 12, 2)->nullable();
             $table->decimal('remaining_amount', 12, 2);
-
             $table->enum('status', [
                 'pending',
                 'partially_paid',
                 'paid',
             ])->nullable()->index();
-
             $table->timestamps();
             $table->softDeletes();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+// API routes for user management for 7 functions: index, show, store, update, destroy, show_for_roles
+Route::resource('users', UserController::class)
+    ->only(['index', 'show']);
+
+// Show user with roles 
+Route::get('/users/role/{role}', [UserController::class, 'showByRole']);  
+
+
 
 
 
