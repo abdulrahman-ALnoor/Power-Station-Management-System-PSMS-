5b80db07f61a720cbcd5f7d2deb657a1cb4f1335
MohKhushafa <makhushafa@gmail.com>
2026-07-28 17:10:55 +0300

    Add Customer and CompanyProfile controllers and set up index and show routes

--- app/Models/equipment.php
+++ app/Models/equipment.php
@@ -2,13 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class equipment extends Model
+class Equipment extends Model
 {
-    use HasFactory;
+    //
 
-    protected $fillable = ['id', 'user_id', 'equipment_name', 'serial_number', 'status', 'notes', 'created_by','created_at','updated_at','deleted_at'];
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'equipment_name',
+        'serial_number',
+        'status',
+        'notes',
+        'created_by',
+    ];
+
+
+    // the casts() function defines the data types for specific attributes of the Equipment model.
+    // It ensures that the attributes are automatically cast to the specified data types when accessed or set
+    // the realationship is one-to-many, where one equipment can have many attributes.
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // the creator() function defines a belongsTo relationship with the User model.
+    // It allows you to access the user who created the equipment record.
+    // the relationship is one-to-many, where one user can create many equipment records.
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
