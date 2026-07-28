be4946eb2bdf3a7aaaa4cebfce5fbfce1fdce8f5
AbdulbaqiMukhtarAl-Hakimi <abdulbaqimukhtaralhakimi@gmail.com>
2026-07-28 21:20:18 +0200

    إضافة متحكمات الفواتير وقراءات العدادات وربط المسارات

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\MeterReadingController;
 
 Route::get('/', function () {
+    
     return view('welcome');
 });
+Route::get('/invoices', [InvoiceController::class, 'index'])
+    ->name('invoices.index');
+
+Route::get('/invoices/{id}', [InvoiceController::class, 'show'])
+    ->name('invoices.show');
+
+Route::get('/meter-readings', [MeterReadingController::class, 'index'])
+    ->name('meter-readings.index');
+
+Route::get('/meter-readings/{id}', [MeterReadingController::class, 'show'])
+    ->name('meter-readings.show');
 
 
 
